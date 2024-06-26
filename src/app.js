b3b1fe65ab3457f65cf04202c54fb3c491605012
Developer-of-Stuff <agulley@protonmail.com>
2024-06-26 00:19:26 -0400

    added server/amplify

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '/about.html'));
+})
+
+app.get('/listings', (req, res) => {
+    res.sendFile(path.join(__dirname, '/listings.html'));
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
